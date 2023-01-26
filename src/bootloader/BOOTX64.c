252f18458cebd03977f06ac4545b032b5a16febb
Wereii <8504482+wereii@users.noreply.github.com>
2023-01-26 17:36:31 +0100

    Wip

--- src/bootloader/BOOTX64.c
+++ src/bootloader/BOOTX64.c
@@ -6,6 +6,8 @@
   if (C_EFI_ERROR(s)) \
     return s;
 
+#define MAIN_PRINT(msg) \
+  ERR_CHECK(Print(SystemTable, msg L"\n"));
 
 CEfiStatus
 Print(CEfiSystemTable *st, CEfiChar16 *msg)
@@ -19,8 +21,54 @@ CEfiStatus efi_main(CEfiHandle EfiHandle, CEfiSystemTable *SystemTable)
 
   status = SystemTable->con_out->clear_screen(SystemTable->con_out);
   ERR_CHECK(status)
-  status = Print(SystemTable, L"BOOTX INIT\n");
-  ERR_CHECK(status)
+  MAIN_PRINT(L"BOOTX INIT");
 
-  return 0;
+  CEfiGraphicsOutputProtocol *gop;
+  { // -- Graphics
+    CEfiGuid gopGuid = C_EFI_GRAPHICS_OUTPUT_PROTOCOL_GUID;
+
+    status = SystemTable->boot_services->locate_protocol(&gopGuid, NULL, (void **)&gop);
+    if (C_EFI_ERROR(status))
+    {
+      MAIN_PRINT(L"LOCATE GOP FAIL");
+      return status;
+    }
+    else
+    {
+      MAIN_PRINT(L"LOCATE GOP SUCCESS");
+    }
+
+    CEfiGraphicsOutputProtocolModeInformation *ModeInfo;
+    CEfiUSize ModeInfoSize;
+
+    status = gop->query_mode(gop, gop->mode == NULL ? 0 : gop->mode->mode, &ModeInfoSize, &ModeInfo);
+    if (status == C_EFI_NOT_STARTED)
+    {
+      MAIN_PRINT(L"QUERY GOP MODE SET 0");
+      status = gop->set_mode(gop, 0);
+    }
+    else if (C_EFI_ERROR(status))
+    {
+      MAIN_PRINT(L"GOP UNABLE TO GET NATIVE MODE")
+      return status;
+    }
+
+    CEfiU32 nativeMode = gop->mode->mode;
+    CEfiU32 numModes = gop->mode->max_mode;
+
+    for (int i = 0; i < numModes; i++)
+    {
+      status = gop->query_mode(gop, i, &ModeInfoSize, &ModeInfo);
+      MAIN_PRINT(L"mode %03d width %d height %d format %x%s",
+                 i,
+                 info->HorizontalResolution,
+                 info->VerticalResolution,
+                 info->PixelFormat,
+                 i == nativeMode ? "(current)" : "");
+    }
+  }
+
+  for (;;)
+    ;
+  return C_EFI_SUCCESS;
 }
---
+++ src/bootloader/include/utils.h
@@ -0,0 +1,14 @@
+#ifndef _BOOTLOADER_UTILS_H
+#define _BOOTLOADER_UTILS_H
+
+const char *_hexbase = "0123456789ABCDEF";
+
+inline void
+itohexa(unsigned long i, char *out)
+{
+    for (int i=0; i < sizeof(unsigned long); i++){
+        
+    }
+}
+
+#endif //_BOOTLOADER_UTILS_H
